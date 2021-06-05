ea12cb871044d1656abc4f9bc6c4c8cd8b716c0f
Sstark97 <aitorscinfo@gmail.com>
2021-06-05 09:15:57 +0100

    11º Class

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: 'main.js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]' 
     },
     resolve: {
         extensions: ['.js']
@@ -31,6 +32,20 @@ module.exports = {
             }, {
                 test: /\.png/,
                 type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options : {
+                        limit: 10000,
+                        mimetype: "application/font-woff",
+                        name: "[name].[ext]",
+                        outputPath: "./assets/font",
+                        publicPath: ".assets/fonts",
+                        esModule: false
+                    }
+                }
             }
         ]
     },
