e09445a127ed760921d6f54967ef69af81495fb4
Sstark97 <aitorscinfo@gmail.com>
2021-06-05 11:27:32 +0100

    Change in HTML plugin 1.3

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const Terserplugin = require('terser-webpack-plugin');
 const DotEnv = require('dotenv-webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { webpack } = require('webpack');
 
 module.exports = {
     entry: './src/index.js',
@@ -96,7 +97,10 @@ module.exports = {
             ]
         }),
         new DotEnv(),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.API': JSON.stringify(process.env.API),
+          })
     ],
     optimization: {
         minimize: true,
