df1e9ecf1d69f28097e7a7322a65eaa451da85c3
Sstark97 <aitorscinfo@gmail.com>
2021-06-05 13:55:58 +0100

    19 Class

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,7 +13,6 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]' 
     },
     mode: 'development',
-    watch:true,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -79,5 +78,11 @@ module.exports = {
             ]
         }),
         new DotEnv(),
-    ]
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3006
+    }
 }
